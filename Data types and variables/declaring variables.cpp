26f6219b0816bcd8273bb018c174b00562f4b02f
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-09 09:20:52 +0200

    Create declaring variables.cpp

---
+++ Data types and variables/declaring variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  /*
+   1-Declare
+   2-assign
+   3-get
+   */
+  // it's a declare a variables
+  int number1;
+  int number2;
+
+  // assign a variables
+  number1=10;
+  number2=20;
+
+  // get a values
+  cout<<"number1 ="<<number1<<endl;
+  cout<<"number2 ="<<number2<<endl;
+    return 0;
+}
