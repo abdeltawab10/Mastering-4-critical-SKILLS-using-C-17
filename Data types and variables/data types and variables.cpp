79f4a3f0675207f566931070be1d0f1bb76c3dc0
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-09 09:11:42 +0200

    Create data types and variables.cpp

---
+++ Data types and variables/data types and variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+   int age=20 ; // int for numbers
+   double d=186.0;  // double for decimal number has a size 8bits
+   float f=10.6;  // float for decimal number has a size 4 bits
+   bool gender= true; // has a two status true or false
+   char ch='a';  // for a character 
+   string name="abdeltawab"; // string is a sequence of char // or a word
+    return 0;
+}
