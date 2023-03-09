48342a2dae921da71c4f469738da2c8fa03b51c8
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-09 09:39:17 +0200

    Create constant variables.cpp

---
+++ Data types and variables/constant variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   // use a caps letter
+   const double PI=3.14;
+   // PI don't change cuz it's a constant var
+   PI=13.5;
+   // it's a error
+    return 0;
+}
