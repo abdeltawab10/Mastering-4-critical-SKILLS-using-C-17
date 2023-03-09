d88bb444b518df6ba637440d36abca09baf48c41
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-09 09:32:33 +0200

    Create reading variables.cpp

---
+++ Data types and variables/reading variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a ,b;
+   cout<<"Enter two numbers \n";
+
+   cin>>a>>b;  // cin fpr reading a variables
+   cout<<a*b<<a+b;
+    return 0;
+}
