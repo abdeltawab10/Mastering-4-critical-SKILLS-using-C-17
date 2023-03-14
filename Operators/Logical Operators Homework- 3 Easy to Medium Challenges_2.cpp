b3c03f201919e7eaaf201b6a046dc31cdb601dcf
Abdeltawab Wagieh <98906055+abdeltawab10@users.noreply.github.com>
2023-03-14 12:30:00 +0200

    Create Logical Operators Homework- 3 Easy to Medium Challenges_2.cpp

---
+++ Operators/Logical Operators Homework- 3 Easy to Medium Challenges_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int a=10,b=20,c=30,d=40;
+    cout<<(a+b==c)<<"\n";
+    cout<<(a+b+c>=2 *d)<<"\n";
+    cout<<(a>5 || d<30)<<"\n";
+    cout<<(a>5 && d<30)<<"\n";
+    cout<<(a<=b && b<=c)<<"\n";
+    cout<<(a>5 && d<30|| c-b==10)<<"\n";
+    cout<<(a<=b && b<=c && c<=d)<<"\n";
+    cout<<(a>5 && d<30 ||c>d ||d%2==0)<<"\n";
+    cout<<(a>5 && d<30 ||c>d && d%2==0)<<"\n";
+
+
+
+    return 0;
+}
